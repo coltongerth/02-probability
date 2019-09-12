fa318418fb78dc28d14cc94f2fd8e89a677c505a
Douglas Brinkerhoff <douglas.brinkerhoff@gmail.com>
2019-09-12 12:13:31 -0600

    made mvt experiment discrete

--- normal_demo.py
+++ normal_demo.py
@@ -6,7 +6,7 @@ n_genes = 100
 n_samples = 100000
 mean_height = 170
 
-effects = np.random.rand(n_genes)*10 - 5
+effects = np.random.randint(-5,6,n_genes)
 heights = []
 
 for n in range(n_samples):
@@ -14,5 +14,5 @@ for n in range(n_samples):
     height_deviation = np.sum(genes*effects)
     heights.append(mean_height + height_deviation)
 
-plt.hist(heights,50)
+plt.hist(heights,bins=np.arange(100,250,1))
 plt.show()
